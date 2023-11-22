edb1e66772e351c6d4fa54f4227e105c09ff1fde
denisvaneev <vaneev.denis98@yandex.ru>
2023-11-22 20:35:21 +0500

    Correction .env 4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_stub_key')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'True'
 
